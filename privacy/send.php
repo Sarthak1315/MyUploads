d7e7c56b6300ca2257b10d1fadeaba6b366fecc2
Sarthak Patel <75411376+Sarthak1315@users.noreply.github.com>
2024-04-22 12:10:30 +0530

    Add files via upload

---
+++ privacy/send.php
@@ -0,0 +1,112 @@
+<?php 
+include "conn.php";
+$obj=new con();
+$con = $obj->c;
+$res=$obj->welcome_user("sarthakpatel1315@gmail.com","sarthak");
+if($res){
+        echo $res;
+    }
+// if(isset($_POST["sub"])){
+//     $res = $obj->send_e($_POST["email"],$_POST["user"]);
+//     if($res){
+//         echo $res;
+//     }
+// }else
+// {
+//     echo "not sub";
+// $sql="select * from login";
+// $result = $con->query($sql);
+        // $data=$result->fetch_assoc();
+        // print_r($data);
+        // while($data=$result->fetch_assoc()){
+        //     if(!empty($data["email_id"])){
+        //         $res = $obj->send_e($data["email_id"],$data["user_name"]);
+        //         if($res){
+        //             echo $data["user_name"].'='.$res;
+        //         }
+        //         echo $data["user_name"] .'='.$data["email_id"]."<br>";
+        //     }
+           
+                
+        //     }
+// }
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Welcome to MyUploads</title>
+<style>
+    /* CSS styles for email */
+    h1 {
+        color: #333;
+        margin: 25px;
+    }
+    p {
+        color: #666;
+        margin-bottom: 10px;
+    }
+    .btn {
+        display: inline-block;
+        padding: 15px 100px;
+	border:1px solid #007bff;
+        color: #007bff;
+        text-decoration: none;
+        border-radius: 5px;
+        margin-top: 15px;
+	
+    }
+    .btn:hover {
+        background-color: #007bff;
+	color : white;
+    }
+</style>
+</head>
+<body style="font-family: Helvetica, Arial, sans-serif; margin: 0px; padding: 0px; background-color: #ffffff;">
+    <table role="presentation"
+    style="width: 100%; border-collapse: collapse; border: 0px; border-spacing: 0px; font-family: Arial, Helvetica, sans-serif; background-color: rgb(239, 239, 239);">
+    <tbody>
+      <tr>
+        <td align="center" style="padding: 1rem 2rem; vertical-align: top; width: 100%;">
+          <table role="presentation" style="max-width: 600px; border-collapse: collapse; border: 0px; border-spacing: 0px; text-align: left;">
+            <tbody>
+              <tr>
+                <td style="padding: 5px 0px 0px;">
+                  <div style="text-align: center; font-size:large;">
+                    <div style="padding-bottom: 20px;"><a href="https://thetechocean.me" style="text-decoration: none; color: #000;"><h1>Tech Ocean</h1></a>
+                    </div>
+                  </div>
+                   <div style="padding: 20px; background-color: rgb(255, 255, 255);">
+                    <div style="color: rgb(0, 0, 0); text-align: left;">
+                        <center><h1>Welcome to MyUploads</h1></center>
+                        <img src="Uploading-amico.svg" alt="img">
+                        <p>Dear '.$user.',</p>
+                        <p>Thank you for joining MyUploads! We\'re excited to have you as a new member of our community.</p>
+                        <p>You can now log in to your account and start uploading and managing your files with ease.</p>
+                    <center><a href="https://myuploads.thetechocean.me" target="_blank" class="btn">Log In</a></center>
+                        <p>If you have any questions or need assistance, feel free to contact our support team.</p>
+                        
+                        <p>Happy uploading!</p>
+                        
+                      <p style="padding-bottom: 16px">Best regards,<br>Sarthak Patel<br><a href = "https://thetechocean.me">Tech Ocean</a></p>
+                    </div>
+                  </div>
+                  <div style="padding-top: 20px; color: rgb(153, 153, 153); text-align: center;">
+                    <p style="padding-bottom: 16px">Made By Sarthak Patel</p>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+
+</body>
+</html>
---
+++ privacy/send_index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index_sendmail</title>
+</head>
+<body>
+    <form action="./send.php" method="post">
+    user <input type="txt" name="user"><br><br>
+        email <input type="email" name="email"><br><br>
+     <input type="submit" value="Submit" name="sub">
+    </form>
+</body>
+</html>
